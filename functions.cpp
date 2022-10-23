617e1a80d054781df5596370e7cb8ab2e023f8a2
Ivan <ivans888888@mail.ru>
2022-10-23 19:29:57 +0300

    Addd functions addition

---
+++ functions.cpp
@@ -0,0 +1,6 @@
+#include "functions.h"
+
+double Addition(double a, double b)
+{
+    return a + b;
+}
---
+++ functions.h
@@ -0,0 +1,4 @@
+#ifndef FUNCTIONS
+#define FUNCTIONS
+double Addition(double, double);
+#endif // FUNCTIONS
